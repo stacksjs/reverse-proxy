1e5c63c0c49a8651d06b2b4592027e23b1b1e246
Chris <chrisbreuer93@gmail.com>
2024-10-08 17:18:38 +0200

    chore: adjust build process

--- build.ts
+++ build.ts
@@ -1,12 +1,8 @@
-import path from 'node:path'
 import { log } from '@stacksjs/logging'
 import { $ } from 'bun'
 import dts from 'bun-plugin-dts-auto'
 
 log.info('Building...')
-
-$.cwd(path.resolve(import.meta.dir, '..'))
-await $`rm -rf ./dist`
 
 await Bun.build({
   entrypoints: ['./src/index.ts', './bin/cli.ts'],
@@ -14,11 +10,8 @@ await Bun.build({
   format: 'esm',
   target: 'bun',
   external: ['rollup', 'fsevents'],
-
   plugins: [
-    dts({
-      cwd: path.resolve(import.meta.dir, '..'),
-    }),
+    dts(),
   ],
 })
 
@@ -26,7 +19,7 @@ await $`cp ./dist/src/index.js ./dist/index.js`
 await $`rm -rf ./dist/src`
 await $`cp ./dist/bin/cli.js ./dist/cli.js`
 await $`rm -rf ./dist/bin`
-await $`cp ./bin/cli.d.ts ./dist/cli.d.ts` // while bun-plugin-dts-auto doesn't support bin files well
+await $`cp ./bin/cli.d.ts ./dist/cli.d.ts`
 await $`rm ./bin/cli.d.ts`
 
 log.success('Built')
