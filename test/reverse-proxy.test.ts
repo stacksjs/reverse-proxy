c9341a45eedb9e1449ccefa1ac8eed96fb2c0749
Chris <chrisbreuer93@gmail.com>
2024-10-08 15:36:08 +0200

    chore: wip

--- test/reverse-proxy.test.ts
+++ test/reverse-proxy.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'bun:test'
 
-describe('should', () => {
-  it('exported', () => {
+describe('@stacksjs/reverse-proxy', () => {
+  it('works', () => {
     expect(1).toEqual(1)
   })
 })
