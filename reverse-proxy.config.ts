d4745fc62cc2502417221c20e5e800a9547451ca
Chris <chrisbreuer93@gmail.com>
2024-10-08 17:13:33 +0200

    chore: adjust examples

--- reverse-proxy.config.ts
+++ reverse-proxy.config.ts
@@ -1,6 +1,6 @@
 export default {
-  'localhost:3000': 'stacks.localhost',
-  // 'localhost:3001': 'docs.stacks.localhost',
+  'localhost:5173': 'stacks.localhost',
+  // 'localhost:3333': 'docs.stacks.localhost',
   // 'localhost:8080': 'api.stacks.localhost',
   // 'localhost:1234': 'example.test',
 }
