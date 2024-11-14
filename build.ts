a2453d1b2dc90b782eb379da52c9ed58be07e564
Chris <chrisbreuer93@gmail.com>
2024-11-14 11:41:54 +0100

    feat: attach binaries

--- build.ts
+++ build.ts
@@ -14,10 +14,8 @@ await Bun.build({
 })
 
 await $`cp ./dist/src/index.js ./dist/index.js`
-await $`rm -rf ./dist/src`
 await $`cp ./dist/bin/cli.js ./dist/cli.js`
+await $`rm -rf ./dist/src`
 await $`rm -rf ./dist/bin`
-await $`cp ./bin/cli.d.ts ./dist/cli.d.ts`
-await $`rm ./bin/cli.d.ts`
 
 log.success('Built')
--- reverse-proxy.config.ts
+++ reverse-proxy.config.ts
@@ -5,11 +5,7 @@ import path from 'node:path'
 const config: ReverseProxyOptions = {
   from: 'localhost:5173',
   to: 'stacks.localhost',
-  // key: 'content of the key',
-  // keyPath: '/absolute/path/to/the/key',
   keyPath: path.join(os.homedir(), '.stacks', 'ssl', `stacks.localhost.crt.key`),
-  // cert: 'content of the cert',
-  // certPath: '/absolute/path/to/the/cert',
   certPath: path.join(os.homedir(), '.stacks', 'ssl', `stacks.localhost.crt`),
   caCertPath: path.join(os.homedir(), '.stacks', 'ssl', `stacks.localhost.ca.crt`),
   httpsRedirect: false,
