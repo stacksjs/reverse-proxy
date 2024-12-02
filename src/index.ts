c064d8d729b0141ad3ef073e9b55d1904ae8df47
Chris <chrisbreuer93@gmail.com>
2024-12-02 12:15:06 +0100

    chore: add default export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import { startProxies } from './start'
+
 export { config } from './config'
 export * from './hosts'
 export * from './https'
 export * from './start'
 export * from './types'
 export * from './utils'
+
+export default startProxies
