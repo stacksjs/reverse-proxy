eadc18759ac7e2adb949b85d73c6a70055384a3e
Chris <chrisbreuer93@gmail.com>
2024-12-09 21:06:29 +0100

    chore: add default docs theme

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,21 @@
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+// https://vitepress.dev/guide/custom-theme
+import { h } from 'vue'
+import './docs.css'
+
+// import TwoSlashFloatingVue from 'vitepress-plugin-twoslash/client'
+
+export default {
+  extends: DefaultTheme,
+
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
+  },
+
+  // enhanceApp({ app }: EnhanceAppContext) {
+  //   app.use(TwoSlashFloatingVue)
+  // },
+} satisfies Theme
